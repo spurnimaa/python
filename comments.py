17d36c7217ad28f4335b346be18eb28d995ea7e3
spurnimaa <110391063+spurnimaa@users.noreply.github.com>
2022-08-02 14:39:30 +0530

    Create comments.py

---
+++ comments.py
@@ -0,0 +1,8 @@
+#single line comment in python
+print("Hello World!")
+#print("comments")
+"""multiple line comments in python
+This is a comment written more than 
+one line"""
+"""print("Hai,this is an Interpreted Language")"""
+print("multiple line comments")
